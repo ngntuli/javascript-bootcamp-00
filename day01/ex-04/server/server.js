fc94bd8047e3027edcff71b9945a848f2257b52c
ngntuli <mr.ng.ntuli@gmail.com>
2023-01-16 10:03:46 +0200

    server file

--- day01/ex-04/server/server.js
+++ day01/ex-04/server/server.js
@@ -1,15 +1,28 @@
 import config from '../config/config.js'
 import express from 'express'
-let template = ejs.compile()
+import { Router } from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
 
 const app = express()
+const router = Router()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
+app.set('views', path.resolve(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.engine('html', require('ejs').renderFile)
-
-app.get('/name', (req, res) => {
-    res.send("Test")
+router.get('/name', (req, res) => {
+    let data = 'Hello Unkown'
+    res.render('index', {data})
 })
+router.get('/name/:name', (req, res) => {
+    let name = req.params.name
+    let data = 'Hello ' + name
+    res.render("index", {data})
+})
+
+app.use('/', router)
 
 app.listen(config.port, console.log(`http://${config.host}:${config.port}`))
